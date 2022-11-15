464c9d4a595a6f77a018f8bfde5da9abb0017fef
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2022-11-15 22:45:22 +0100

    Banner

--- code/rentATractor/rentATractor/urls.py
+++ code/rentATractor/rentATractor/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
+import django.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.index),
+    path('media/<path>', django.views.static.serve,
+         {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
