0fc4679c1b192f1bd8f34cb5a6cedd43ab6fbe71
ENRIQUE PRADA VAZQUEZ <enrpravaz@alum.us.es>
2022-11-24 19:10:19 +0100

    Revert "Actualización de rama"

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # PGPI-G3.2
 Los comandos para inicializar y ejecutar el proyecto son:
-- ```python manage.py makemigrations```
 - ```python manage.py migrate```
 - ```python manage.py runserver```
-
-La aplicación se puede encontrar en http://enprava.pythonanywhere.com/ 
--- code/rentATractor/app/admin.py
+++ code/rentATractor/app/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from app.models import Maquina, Cliente, Pedido, Tarjeta, ClienteRegistrado, Opinion, Reclamacion, Descuento, Contiene, EnCesta
 
-admin.site.register(Maquina)
-admin.site.register(Cliente)
-admin.site.register(Pedido)
-admin.site.register(Tarjeta)
-admin.site.register(ClienteRegistrado)
-admin.site.register(Opinion)
-admin.site.register(Reclamacion)
-admin.site.register(Descuento)
-admin.site.register(Contiene)
-admin.site.register(EnCesta)
+# Register your models here.
